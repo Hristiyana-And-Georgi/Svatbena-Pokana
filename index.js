12ea8565b595c4e2668f9ad56e2bd8ee433f42e6
Hristiyana-And-Georgi <122784746+Hristiyana-And-Georgi@users.noreply.github.com>
2023-01-16 11:51:27 +0200

    Files uploaded 16.01.2023

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector(".btnAttendance").addEventListener("click", handleClick);
+
+function handleClick() {
+  window.open("rsvp.html");
+}
+
+data.append("email", document.getElementById("Form").value);
