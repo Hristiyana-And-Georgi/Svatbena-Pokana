a9db52a127036e3d293dc082ebdbed7fa739b048
Hristiyana-And-Georgi <122784746+Hristiyana-And-Georgi@users.noreply.github.com>
2023-01-17 11:29:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ data.append("email", document.getElementById("Form").value);
 // Test for Thank you rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65
 
 
-      const scriptURL = "https://hristiyana-and-georgi.github.io/Svatbena-Pokana/rsvp.html"
+      const scriptURL = ""
       const form = document.forms["formName"]
 
       form.addEventListener("submit", e => {
