ae8d98e6945a1b17b04a167ef71a81e2c4ac8ed4
Hristiyana-And-Georgi <122784746+Hristiyana-And-Georgi@users.noreply.github.com>
2023-01-17 11:24:56 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ function handleClick() {
 }
 
 data.append("email", document.getElementById("Form").value);
+
+
+// Test for Thank you rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65
+
+
+      const scriptURL = 'file:///C:/Users/hrist/OneDrive/%D0%A0%D0%B0%D0%B1%D0%BE%D1%82%D0%B5%D0%BD%20%D0%BF%D0%BB%D0%BE%D1%82/Web%20Dev%20Course/Wedding-Invitation-Website'
+      const form = document.forms['formName']
+
+      form.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => alert("Thank you! your form is submitted successfully." ))
+        .then(() => {  window.location.reload(); })
+        .catch(error => console.error('Error!', error.message))
+      })
