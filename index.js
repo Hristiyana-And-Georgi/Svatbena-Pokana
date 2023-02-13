675e3b1c51a7ba4def772ebed7667311dc20683c
Hristiyana-And-Georgi <122784746+Hristiyana-And-Georgi@users.noreply.github.com>
2023-02-13 09:36:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ function handleClick() {
 
 data.append("email", document.getElementById("Form").value);
 
+var checkboxPlusOne = document.querySelector(".testcheck");
+
+checkboxPlusOne.addEventListener("checked", function {
+  if (checkboxPlusOne.checked){
+    alert("it works")
+    document.getElementById("name+1").removeAttribute("disabled");
+    document.getElementById("menu+1").removeAttribute("disabled");
+  } else {
+    alert("No");
+    document.getElementById("name+1").setAttribute("disabled");
+    document.getElementById("menu+1").setAttribute("disabled");
+  }
+}
