2b91e0d13bd9cb3ef0e49d905208bccde08d6bf5
Hristiyana-And-Georgi <122784746+Hristiyana-And-Georgi@users.noreply.github.com>
2023-03-15 15:47:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ function handleClick() {
 
 data.append("email", document.getElementById("Form").value);
 
-
+			form.addEventListener('submit', e => {
+			  e.preventDefault()
+			  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+				.then(response => alert("Thank you! your form is submitted successfully." ))
+				.then(() => {  window.location.reload(); })
+				.catch(error => console.error('Error!', error.message))
+			})
