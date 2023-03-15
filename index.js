213a0e07c768e11041eb5d8fa0ac562f583c6676
Hristiyana-And-Georgi <122784746+Hristiyana-And-Georgi@users.noreply.github.com>
2023-03-15 16:42:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,23 @@ function handleClick() {
   window.open("rsvp.html");
 }
 
-const scriptURL = ''
-			const form = document.forms['contactform']
+document.querySelector(".btn").addEventListener("click", handleClick2);
+
+function handleClick2() {
+  window.open("thankyoupage.html");
+}
+
+
+// const scriptURL = ''
+// 			const form = document.forms['contactform']
 		  
-			form.addEventListener('submit', e => {
-			  e.preventDefault()
-			  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-			.then(response => alert("Thank you! your form is submitted successfully." ))
-			.then(() => {  window.location.reload(); })
-			.catch(error => console.error('Error!', error.message))
-			});
+// 			form.addEventListener('submit', e => {
+// 			  e.preventDefault()
+// 			  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+// 			.then(response => alert("Thank you! your form is submitted successfully." ))
+// 			.then(() => {  window.location.reload(); })
+// 			.catch(error => console.error('Error!', error.message))
+// 			});
 
 data.append("email", document.getElementById("Form").value);
 
